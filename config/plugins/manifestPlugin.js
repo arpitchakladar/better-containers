fcb4cbd42c18f74a98f00802bdd0a9419b4e7bd0
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-04-05 00:33:49 +0530

    Using remeda in manifestPlugin.

--- config/plugins/manifestPlugin.js
+++ config/plugins/manifestPlugin.js
@@ -1,3 +1,4 @@
+import * as R from "remeda";
 import fs from "fs";
 import path from "path";
 import { production } from "../env.js";
@@ -12,42 +13,61 @@ export function manifestPlugin(options = {}) {
 	return {
 		name: "manifest-plugin",
 		generateBundle(outputOptions, bundle) {
-			const [webAccessibleResources, backgroundScripts] = Object.keys(
+			const [webAccessibleResources, backgroundScripts] = R.pipe(
 				bundle,
-			).reduce(
-				([webAccessibleResources, backgroundScripts], fileName) => {
-					if (fileName.endsWith(".css") || fileName.endsWith(".html")) {
-						webAccessibleResources.push(fileName);
-					} else if (fileName.endsWith(".js")) {
-						if (fileName.split("/")[0] === "background") {
-							backgroundScripts.push(fileName);
-						} else {
-							webAccessibleResources.push(fileName);
-						}
-					}
-					return [webAccessibleResources, backgroundScripts];
-				},
-				[[], []],
+				R.keys(),
+				R.reduce(
+					([webAccessibleResources, backgroundScripts], fileName) =>
+						R.conditional(
+							fileName,
+							[
+								(fileName) =>
+									fileName.endsWith(".css") || fileName.endsWith(".html"),
+								(fileName) => [
+									webAccessibleResources.concat(fileName),
+									backgroundScripts,
+								],
+							],
+							[
+								(fileName) => fileName.endsWith(".js"),
+								(fileName) =>
+									R.conditional(
+										fileName,
+										[
+											(fileName) => fileName.split("/")[0] === "background",
+											(fileName) => [
+												webAccessibleResources,
+												backgroundScripts.concat(fileName),
+											],
+										],
+										R.conditional.defaultCase((fileName) => [
+											webAccessibleResources.concat(fileName),
+											backgroundScripts,
+										]),
+									),
+							],
+							R.conditional.defaultCase((_fileName) => [
+								webAccessibleResources,
+								backgroundScripts,
+							]),
+						),
+					[[], []],
+				),
 			);
 			const packageJson = JSON.parse(
 				fs.readFileSync(path.join(__dirname, "../package.json"), "utf8"),
 			);
 
-			const manifest = {
-				...baseManifest,
-				version: packageJson.version,
-				background: {
-					...(baseManifest.background || {}),
-					scripts: [
-						...(baseManifest.background.scripts || []),
-						...backgroundScripts,
-					],
+			const manifest = R.mergeDeep(
+				baseManifest,
+				{
+					version: packageJson.version,
+					background: {
+						scripts: backgroundScripts
+					},
+					web_accessible_resources: webAccessibleResources,
 				},
-				web_accessible_resources: [
-					...(baseManifest.web_accessible_resources || []),
-					...webAccessibleResources,
-				],
-			};
+			);
 
 			const manifestPath = path.resolve(outputPath);
 			const manifestData = JSON.stringify(manifest, null, production ? 0 : 2);
